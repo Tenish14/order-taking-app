5548c24d3fe55f2c4925d701124196ff996a526e
Tenish <Tenish02@gmail.com>
2022-05-30 17:06:14 +0800

    Setup Layout and Created Scaffold For Items

---
+++ db/migrate/20220530084841_create_items.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
